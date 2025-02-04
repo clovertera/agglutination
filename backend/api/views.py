1eac0a5f3409bf1a75dbf3e25c25aa508179b665
clovertera <thelenvr@gmail.com>
2025-02-03 22:02:36 -0500

    added regex to parse the output from morphological single word analysis from zemberek

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+﻿from django.shortcuts import render
 from django.http import JsonResponse
 from zemberek import TurkishMorphology
+import re, os
 
 LANGUAGES = {
 	"Turkish": "tr",
@@ -11,7 +12,7 @@ morphology = TurkishMorphology.create_with_defaults()
 def decompose_word(request):
 	word = request.GET.get('word', '')
 	language = request.GET.get('language', '')
-	# need to use stanza to get the components of the requested word
+	# need to use zemberek to get the components of the requested word
 	# use the given language
 	# placeholder: components = ["example", "decomposition"]
 	components = get_components(word, 'Turkish')
@@ -23,4 +24,35 @@ def get_components(word, language):
 	results_list = []
 	for result in results:
 		results_list.append(str(result))
+		make_human_readable(str(result))
 	return results_list
+
+def make_human_readable(interpretation):
+	# interpretation example: 
+	# [kalem:Noun] kalem:Noun+A3sg+in:Gen
+
+	interpretation = re.split(r'[\[\]]+', interpretation)
+	interpretation = [word for word in interpretation if word]
+	interpretation[1] = interpretation[1].lstrip()
+
+	print("Performing regex on this string: " + interpretation[1])
+	word_feat = r"(\w+:(?:\w+→\w+|\w+))" # word:feature or word:feature->feature
+	pattern = r"{}(?:\+{})*(?:\|{})?".format(word_feat, word_feat, word_feat)
+
+	regex_matches = re.findall(pattern, interpretation[1])
+	dict = {}
+	for match in regex_matches:
+		for elem in match:
+			if elem:
+				elem = elem.split(':')
+				dict[elem[0]] = elem[1]
+	print(dict)
+
+	path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+	path = os.path.join(path, 'human_readable_features.txt')
+	human_readable_file = open(path)
+
+	# match each feature with feature value name and output a human readable description/blurb
+
+	human_readable_file.close()
+	
